e84d074a1a00c67146dcb3437c9e3b0994d47726
Edward <edward821220@gmail.com>
2023-11-19 12:44:58 +0800

    move diamond contracts for lib folder to contracts folder

--- contracts/DiamondFactory.sol
+++ contracts/DiamondFactory.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {Diamond} from "diamond/Diamond.sol";
+import {Diamond} from "../contracts/Diamond.sol";
+import {DiamondCutFacet} from "../contracts/facets/DiamondCutFacet.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DiamondFactory is Ownable {
@@ -15,10 +16,12 @@ contract DiamondFactory is Ownable {
 
     fallback() external payable {}
 
-    function createDiamond() external returns (string memory name) {
-        bytes32 _salt = keccak256(abi.encodePacked(name, msg.sender));
-        Diamond diamond = new Diamond{salt: _salt}(msg.sender, address(1));
+    function createDaoDiamond(string calldata name_) external returns (address) {
+        bytes32 _salt = keccak256(abi.encodePacked(name_, msg.sender));
+        DiamondCutFacet diamondCutFacet = new DiamondCutFacet();
+        Diamond diamond = new Diamond{salt: _salt}(msg.sender, address(diamondCutFacet));
         emit DAOCreated(address(diamond), msg.sender);
+        return address(diamond);
     }
 
     function withdraw() external onlyOwner {
