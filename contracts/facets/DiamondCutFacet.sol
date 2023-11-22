6bde81e56e199c626d618e8e2deeef2a5834a6ae
Edward <edward821220@gmail.com>
2023-11-22 18:30:14 +0800

    add diamondCutByProposal function

--- contracts/facets/DiamondCutFacet.sol
+++ contracts/facets/DiamondCutFacet.sol
@@ -3,16 +3,24 @@ pragma solidity ^0.8.0;
 
 import {IDiamondCut} from "../interfaces/IDiamondCut.sol";
 import {LibDiamond} from "../utils/LibDiamond.sol";
+import {AppStorage, Status} from "../utils/AppStorage.sol";
 
 contract DiamondCutFacet is IDiamondCut {
-    /// @notice Add/replace/remove any number of functions and optionally execute
-    ///         a function with delegatecall
-    /// @param _diamondCut Contains the facet addresses and function selectors
-    /// @param _init The address of the contract or facet to execute _calldata
-    /// @param _calldata A function call, including function selector and arguments
-    ///                  _calldata is executed with delegatecall on _init
+    AppStorage internal s;
+
+    // The owner of the contract is set to 0 after the contract is initialized until the members of the DAO upgrade the Ownership feature through a proposal. Before that, any feature upgrades must use the diamondCutByProposal function.
     function diamondCut(FacetCut[] calldata _diamondCut, address _init, bytes calldata _calldata) external override {
         LibDiamond.enforceIsContractOwner();
+        LibDiamond.diamondCut(_diamondCut, _init, _calldata);
+    }
+
+    function diamondCutByProposal(
+        uint256 proposalId,
+        FacetCut[] calldata _diamondCut,
+        address _init,
+        bytes calldata _calldata
+    ) external {
+        require(s.proposals[proposalId].status == Status.Approved, "Proposal not approved");
         LibDiamond.diamondCut(_diamondCut, _init, _calldata);
     }
 }
